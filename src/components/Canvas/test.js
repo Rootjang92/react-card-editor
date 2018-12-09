001dfac635a4945ccbdc38b932700a85e34aa2db
wkddnjset <wkddnjset@naver.com>
2018-12-09 15:14:05 +0900

    Create test file

---
+++ src/components/Canvas/test.js
@@ -0,0 +1,12 @@
+/*
+Story. 사용자는 Canvas를 생성할 수 있다.
+	senario
+		given: 설정없이
+		when: Canvas를 생성하면
+		then: windowSize에 맞춘 scale과 index를 사용해
+					Canvas를 생성한다.
+	senario
+		given: scale과 title을 설정하여
+		when: Canvas를 생성하면
+		then: 설정한 scale과 title에 맞춰진 Canvas가 생성된다.
+*/
